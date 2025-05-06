5ebc4198770aa28743bf8cc7a2a0b141ebcec817
himalf <himal.fullel15@gmail.com>
2025-05-06 09:44:42 +0545

    feat: defines models fo users and blogs

---
+++ controller/blogController.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Blog", blogSchema);
